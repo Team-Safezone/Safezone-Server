a1e1a485e1fd4763d49c97bf69f8ac6ee9aacc50
Kim Yoon Su <yoonsu1717@gmail.com>
2024-05-17 00:30:57 +0900

    🎉 Feat: 경기 타임라인 실시간 크롤링

---
+++ src/main/java/KickIt/server/global/common/crawler/RTCrawler.java
@@ -0,0 +1,123 @@
+package KickIt.server.global.common.crawler;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RTCrawler {
+
+    public static void main(String[] args) {
+        RTCrawler instance = new RTCrawler();
+        while (true) {
+            String finish = instance.processEvents(); // 이벤트 처리 메소드 호출
+            if("경기종료".equals(finish)){
+                return;
+            }
+        }
+    }
+
+    public String processEvents() {
+        boolean eventEnded = false; // 이벤트 종료 여부를 저장하는 변수 추가
+        String finish = null;
+
+        //웹 페이지 이동
+        WebDriver driver = new ChromeDriver();
+        //driver.get("https://sports.daum.net/worldsoccer");
+        //경기 시작 일정 저장 필요(12시 넘어가면 화면 변경)
+        //일정 날짜에 맞는 버튼 클릭하게 코드 수정 예정
+
+        //임시 페이지
+        driver.get("https://sports.daum.net/match/80075512");
+
+        /*
+        // 경기 일정 선택
+        List<WebElement> DateButton = driver.findElements(By.cssSelector("span.txt_subgnb"));
+        DateButton.get(2).click();
+
+
+        // 프리미어리그 선택
+        List<WebElement> Prem = driver.findElements(By.className("link_tab"));
+        for (WebElement button : Prem) {
+            if (button.getText().equals("프리미어리그")) {
+                button.click();
+                break;
+            }
+        }
+
+
+        //
+        //
+        // 지금은 실시간 랜덤 경기 정보 받아옴
+        // 나중에 선호하는 팀에 대한 경기 정보 받아보는 코드 추가 필요
+        //
+        //
+
+
+        // 문자중계 확인 버튼
+        List<WebElement> HighlightButton = driver.findElements(By.className("link_game"));
+        HighlightButton.get(17).click();
+
+
+
+         */
+
+        //이전에 저장된 정보
+        Set<String> previousTDs = new HashSet<>();
+
+        // 변경된 요소를 저장
+        Set<String> newTDs = new HashSet<>();
+
+        try {
+            // 이벤트 업데이트 최소 시간 (30분)
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(30));
+
+            wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(By.className("desc_empty"), "경기 시작 후 업데이트됩니다.")));
+
+            while (!eventEnded) { // 이벤트가 종료되지 않은 동안 반복
+                // td 요소 찾기
+                WebElement TimeLine = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("group_timeline")));
+                List<WebElement> TimeElements = TimeLine.findElements(By.tagName("li"));
+
+                // 변경된 td 요소 찾기
+                for (WebElement td : TimeElements) {
+                    String tdText = td.getText();
+                    if (!previousTDs.contains(tdText)) {
+                        newTDs.add(tdText);
+                        // 변경된 내용 출력
+                        System.out.println(tdText);
+                    }
+                }
+
+                // 이전 td 요소들 업데이트
+                previousTDs.clear();
+                for (WebElement td : TimeElements) {
+                    previousTDs.add(td.getText());
+                }
+
+                // 이벤트 종료 확인
+                for (WebElement td : TimeElements) {
+                    String tdText = td.getText();
+                    if (tdText.contains("경기종료")) {
+                        eventEnded = true;
+                        finish ="경기종료"; // 이벤트 종료됨을 표시
+                        break;
+                    }
+                }
+            }
+        } finally {
+            // WebDriver 종료
+            driver.quit();
+        }
+
+        return finish;
+    }
+}
+
