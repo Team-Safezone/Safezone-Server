0f569c7a673d90cf6849e4e40faed1fe617bd6c3
1ny0ung <0828_iyiy@naver.com>
2024-05-17 02:39:28 +0900

    🎉 Feat: 선발 라인업 크롤링을 위한 경기 상세 페이지 url 정보 추가

--- src/main/java/KickIt/server/domain/fixture/entity/Fixture.java
+++ src/main/java/KickIt/server/domain/fixture/entity/Fixture.java
@@ -35,6 +35,8 @@ public class Fixture {
     // 경기 종료: 0 전반전: 1 하프 타임: 2 후반전: 3 경기 전: 4 연기(예외): 5
     // 연기, 취소, 우천 등 예외는 3으로 처리하되 경기 데이터 저장하기로 변경!
     private int status;
+    // 선발 라인업 크롤링해 올 경기 상세 페이지 Url 정보
+    private String lineupUrl;
 }
 
 
