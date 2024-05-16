42c1c6a26e2e799c45fb7a6db98d262547aa5a20
Kim Yoon Su <yoonsu1717@gmail.com>
2024-05-17 02:04:13 +0900

    🎉 Feat: 실시간 타임라인 크롤링 종료 기능

--- src/main/java/KickIt/server/global/common/crawler/RTCrawler.java
+++ src/main/java/KickIt/server/global/common/crawler/RTCrawler.java
@@ -14,51 +14,20 @@ import java.util.Set;
 
 public class RTCrawler {
 
-    public static void main(String[] args) {
-        RTCrawler instance = new RTCrawler();
-        while (true) {
-            String finish = instance.processEvents(); // 이벤트 처리 메소드 호출
-            if("경기종료".equals(finish)){
-                return;
-            }
-        }
-    }
-
-    public String processEvents() {
-        boolean eventEnded = false; // 이벤트 종료 여부를 저장하는 변수 추가
-        String finish = null;
-
+    // 실시간 타임라인을 가져와 출력하는 crawlingRT 함수
+    public String crawlingRT() {
         //웹 페이지 이동
         WebDriver driver = new ChromeDriver();
-        //driver.get("https://sports.daum.net/worldsoccer");
-        //경기 시작 일정 저장 필요(12시 넘어가면 화면 변경)
-        //일정 날짜에 맞는 버튼 클릭하게 코드 수정 예정
-
-        //임시 페이지
-        driver.get("https://sports.daum.net/match/80075512");
+        // 임시 페이지 지정 이동
+        driver.get("https://sports.daum.net/match/80085079");
 
         /*
+
+        // 구현 수정 예정
+        driver.get("https://sports.daum.net/schedule/epl");
+
+
         // 경기 일정 선택
-        List<WebElement> DateButton = driver.findElements(By.cssSelector("span.txt_subgnb"));
-        DateButton.get(2).click();
-
-
-        // 프리미어리그 선택
-        List<WebElement> Prem = driver.findElements(By.className("link_tab"));
-        for (WebElement button : Prem) {
-            if (button.getText().equals("프리미어리그")) {
-                button.click();
-                break;
-            }
-        }
-
-
-        //
-        //
-        // 지금은 실시간 랜덤 경기 정보 받아옴
-        // 나중에 선호하는 팀에 대한 경기 정보 받아보는 코드 추가 필요
-        //
-        //
 
 
         // 문자중계 확인 버튼
@@ -66,49 +35,57 @@ public class RTCrawler {
         HighlightButton.get(17).click();
 
 
+        // 수정 예정
+           경기 시작 일정 저장 (12시 넘어가면 화면 변경 되기 때문)
+           일정 날짜에 맞는 버튼 클릭하게 코드 수정
+           경기 시작 -> 자정 넘어감 -> 사용자 입장 -> 오류..
 
-         */
+           지금은 실시간 랜덤 경기 정보 받아옴
+           나중에 선호하는 팀에 대한 경기 정보 받아보는 코드 추가 필요
+        */
+
 
         //이전에 저장된 정보
-        Set<String> previousTDs = new HashSet<>();
+        Set<String> previousList = new HashSet<>();
 
         // 변경된 요소를 저장
-        Set<String> newTDs = new HashSet<>();
+        Set<String> newList = new HashSet<>();
+
+        // 이벤트 종료 여부를 저장하는 변수 eventEnd
+        boolean eventEnd = false;
 
         try {
             // 이벤트 업데이트 최소 시간 (30분)
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(30));
 
+            // "경기 시작 후 업데이트 됩니다." 화면이 없어질 때까지 대기
             wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(By.className("desc_empty"), "경기 시작 후 업데이트됩니다.")));
 
-            while (!eventEnded) { // 이벤트가 종료되지 않은 동안 반복
-                // td 요소 찾기
-                WebElement TimeLine = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("group_timeline")));
-                List<WebElement> TimeElements = TimeLine.findElements(By.tagName("li"));
+            // 이벤트가 종료되지 않은 동안 반복
+            while (!eventEnd) {
 
-                // 변경된 td 요소 찾기
-                for (WebElement td : TimeElements) {
-                    String tdText = td.getText();
-                    if (!previousTDs.contains(tdText)) {
-                        newTDs.add(tdText);
+                // list 찾기(타임 라인 이벤트가 list 형식으로 되어있음)
+                WebElement timeLine = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("group_timeline")));
+                List<WebElement> timeElements = timeLine.findElements(By.tagName("li"));
+
+                // 변경된 list 요소 탐색
+                for (WebElement li : timeElements) {
+                    String liText = li.getText();
+                    if (!previousList.contains(liText)) {
+                        newList.add(liText);
                         // 변경된 내용 출력
-                        System.out.println(tdText);
+                        System.out.println(liText);
+                        // 요소 저장
+                        previousList.add(li.getText());
                     }
                 }
 
-                // 이전 td 요소들 업데이트
-                previousTDs.clear();
-                for (WebElement td : TimeElements) {
-                    previousTDs.add(td.getText());
-                }
-
                 // 이벤트 종료 확인
-                for (WebElement td : TimeElements) {
-                    String tdText = td.getText();
-                    if (tdText.contains("경기종료")) {
-                        eventEnded = true;
-                        finish ="경기종료"; // 이벤트 종료됨을 표시
-                        break;
+                for (WebElement li : timeElements) {
+                    String liText = li.getText();
+                    if (liText.contains("경기종료")) {
+                        // 경기 끝
+                        eventEnd = true;
                     }
                 }
             }
@@ -117,7 +94,19 @@ public class RTCrawler {
             driver.quit();
         }
 
-        return finish;
+        return (String.valueOf(eventEnd));
     }
+
+
+    public static void main(String[] args) {
+        RTCrawler rtCrawler = new RTCrawler();
+        while (true) {
+            String finish = rtCrawler.crawlingRT();
+            if(finish.equals("true")){
+                return;
+            }
+        }
+    }
+
 }
 
