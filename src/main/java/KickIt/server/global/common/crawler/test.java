82fd5b4888d14a33d1ffa6ca31bc97f420425b9b
1ny0ung <0828_iyiy@naver.com>
2024-05-27 07:11:57 +0900

    ✅ Chore: 이전 테스트 코드 주석 처리 / SquadCrawler 테스트 코드 추가

--- src/main/java/KickIt/server/global/common/crawler/test.java
+++ src/main/java/KickIt/server/global/common/crawler/test.java
@@ -1,33 +1,26 @@
 package KickIt.server.global.common.crawler;
 
-import KickIt.server.domain.fixture.entity.Fixture;
-import KickIt.server.domain.lineup.entity.MatchLineup;
-import KickIt.server.domain.teams.entity.Player;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class test {
 
     public static void main(String[] args) {
 
+        // fixtureCrawler 테스트 및 출력
+        /*
         FixtureCrawler mayFixtureCrawler = new FixtureCrawler();
         String year = String.valueOf(LocalDate.now().getYear());
         String month = String.format("%02d", LocalDate.now().getMonthValue());
         List<Fixture> fixtureList = mayFixtureCrawler.getFixture(year, month);
 
-        // fixtureCrawler 테스트 및 출력
-        /*
         for(int i = 0; i < fixtureList.size(); i++){
             Fixture fixture = fixtureList.get(i);
             Logger.getGlobal().log(Level.INFO, String.format("%s\n%s\n%s\n%s vs %s\n%s : %s\n%sR\n%s\n%s\n",
                     fixture.getId(), fixture.getSeason(), fixture.getDate(), fixture.getHomeTeam(), fixture.getAwayTeam(),
                     fixture.getHomeTeamScore(), fixture.getAwayteamScore(), fixture.getRound(), fixture.getStatus(), fixture.getLineupUrl()));
         }
-        */
-        
+         */
+
+        // LineupCrawler 테스트 및 출력
+        /*
         LineupCrawler lineupCrawler = new LineupCrawler();
         MatchLineup lineup = lineupCrawler.getLineup(fixtureList.get(0));
         if (lineup != null){
@@ -49,6 +42,10 @@ public class test {
                 Logger.getGlobal().log(Level.INFO, String.format("awayteam 후보 선수 명단 \n %s %s %s %s\n", benchPlayer.getId(), benchPlayer.getNumber(), benchPlayer.getName(), benchPlayer.getPosition()));
             }
         }
+         */
+        // PlayerCralwer 테스트 및 출력
+        SquadCrawler squadCrawler = new SquadCrawler();
+        squadCrawler.getTeamSquads();
     }
 }
 
