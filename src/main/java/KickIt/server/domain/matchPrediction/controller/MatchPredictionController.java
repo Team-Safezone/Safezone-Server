ae7e6555a7a40ade47a71b684bc757f4822f76e6
1ny0ung <0828_iyiy@naver.com>
2024-11-08 09:23:41 +0900

    ✅ Chore: 경기 예측 조회 API xAuthToken bearer로 변경

--- src/main/java/KickIt/server/domain/matchPrediction/controller/MatchPredictionController.java
+++ src/main/java/KickIt/server/domain/matchPrediction/controller/MatchPredictionController.java
@@ -10,10 +10,7 @@ import KickIt.server.jwt.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +30,7 @@ public class MatchPredictionController {
     MatchPredictionService matchPredictionService;
 
     @GetMapping()
-    public ResponseEntity<Map<String, Object>> inquireMatchPrediction(@RequestParam("xAuthToken") String xAuthToken, @RequestParam("matchId") Long matchId){
+    public ResponseEntity<Map<String, Object>> inquireMatchPrediction(@RequestHeader(value = "xAuthToken") String xAuthToken, @RequestParam("matchId") Long matchId){
         // 반환할 responseBody
         Map<String, Object> responseBody = new HashMap<>();
         // member를 찾기 위해 token으로 email 조회
